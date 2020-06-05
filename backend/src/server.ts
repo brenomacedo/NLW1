b0268c2acf3c3e1e773a030846c0a3551e8e50bf
brenosekiro <b88660501@gmail.com>
2020-06-05 11:21:04 -0300

    validations

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,12 +2,13 @@ import express from 'express'
 import path from 'path'
 import routes from './routes'
 import cors from 'cors'
+import { errors } from 'celebrate'
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(routes)
-
+app.use(errors)
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 app.listen(3333)
